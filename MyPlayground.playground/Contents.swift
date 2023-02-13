32dd343d977d9c0df9e97ec0feda13266b889f17
ryogabarbie <ryoga1226@gmail.com>
2023-02-13 22:20:20 +0900

    複数から購読できるMultiStreamの実装 storageを配列で持つ形 unchecked sendableのclassになっている。makeAsyncIteratorとfinishはarrayの変更やhandlerのnil化のみなので問題ないが、sendは複数から同時に呼ばれると、購読箇所によってはsendされた順番とは違う順でelementが来る可能性がある。

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -121,43 +121,194 @@ extension  SimpleAsyncThrowingStream {
     }
 }
 
+@available(iOS 16, *)
+public class  SimpleAsyncThrowingMultiStream<Element>: AsyncSequence, @unchecked Sendable  {
+    public typealias Element = Element
+    public typealias AsyncIterator = Iterator
+    public typealias TerminationHandler = @Sendable (Terminal) -> Void
+
+    private var _storages: [Storage] = []
+    private var terminationHandler: TerminationHandler?
+
+    public init(_ terminationHandler: @escaping TerminationHandler) {
+        self.terminationHandler = terminationHandler
+    }
+
+    public func makeAsyncIterator() -> Iterator {
+        let storage = Storage()
+        _storages.append(storage)
+        return Iterator(storage)
+    }
+
+    /// 複数のTaskで同時にsendを叩いだ場合、購読側で実際にsendを叩いた順番とは違う順番でElementが流れてくる可能性がある。
+    public func send(_ element: Element) async {
+        await withTaskGroup(of: Void.self) { group in
+            for _storage in _storages {
+                group.addTask {
+                    await _storage.send(element)
+                }
+            }
+        }
+    }
+
+    public func finish(_ error: Error? = nil) {
+        let handler = terminationHandler
+        terminationHandler = nil
+
+        let terminal: Terminal
+        if let _error = error {
+            terminal = .failed(_error)
+        } else {
+            terminal = .finished
+        }
+
+        handler?(terminal)
+
+        Task.detached {
+            await withTaskGroup(of: Void.self, body: { group in
+                for _storage in self._storages {
+                    group.addTask {
+                        await _storage.finish(terminal)
+                    }
+                }
+            })
+        }
+    }
+}
+
+extension  SimpleAsyncThrowingMultiStream {
+    public struct Iterator: AsyncIteratorProtocol {
+        private let _storage: Storage
+
+        init(_ storage: Storage) {
+            self._storage = storage
+        }
+
+        public func next() async throws -> Element? {
+            try await _storage.next()
+        }
+    }
+}
+
+extension  SimpleAsyncThrowingMultiStream {
+    public enum Terminal {
+        case finished
+        case failed(Error)
+    }
+
+    actor Storage {
+        private struct State {
+            var continuation: CheckedContinuation<Element?, Error>? = nil
+            var pending = [Element]()
+            var terminal: Terminal? = nil
+        }
+
+        private var state: State = .init()
+
+        init() {}
+
+        func next() async throws -> Element? {
+            // finishより前にsendされた値が正しく送信されるか
+            // finishが呼ばれたタイミングで実行されるかを確認する用
+//             try await Task.sleep(nanoseconds: 1_000_000_000)
+            return try await withCheckedThrowingContinuation({ _continuation in
+                if state.pending.count > 0 {
+                    let sendValue = state.pending.removeFirst()
+                    _continuation.resume(returning: sendValue)
+                } else if let termianl = state.terminal {
+                    state.continuation = nil
+
+                    switch termianl {
+                    case .finished:
+                        _continuation.resume(returning: nil)
+                    case .failed(let error):
+                        _continuation.resume(throwing: error)
+                    }
+                } else {
+                    state.continuation = _continuation
+                }
+            })
+        }
+
+        func send(_ element: Element) {
+            if let continuation = state.continuation {
+                state.continuation = nil
+                continuation.resume(returning: element)
+            } else if state.terminal == nil {
+                state.pending.append(element)
+            }
+        }
+
+        func finish(_ terminal: Terminal) {
+            state.terminal = terminal
+
+            if let continuation = state.continuation {
+                state.continuation = nil
+                switch terminal {
+                case .finished:
+                    continuation.resume(returning: nil)
+                case .failed(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
+
+
 enum OriginalError: Error {
     case somethingHappend
 }
 
-let asyncStream =  SimpleAsyncThrowingStream<Int>() { terminal in
+//let asyncStream =  SimpleAsyncThrowingStream<Int>() { terminal in
+let asyncStream =  SimpleAsyncThrowingMultiStream<Int>() { terminal in
     switch terminal {
-    case .finished: print("終了しました")
+    case .finished: print("終了が呼ばれました")
     case .failed(let error):
-        print("\(error)によって失敗しました")
+        print("failed \(error)が呼ばれました")
     }
 }
 
 Task.detached {
     do {
         for try await element in asyncStream {
-            print(element)
+            print("1つ目 \(element)")
         }
     } catch {
-        print("\(error)が発生したのでループを終了します")
+        print("1つ目に\(error)が発生したのでループを終了します")
     }
 }
-
-//Task.detached {
-//    for await notify in notificationCenter {
-//        print("2つ目")
-//        print(notify * 10)
-//    }
-//}
+Task.detached {
+    do {
+        for try await element in asyncStream {
+            print("2つ目 \(element)")
+        }
+    } catch {
+        print("2つ目に\(error)が発生したのでループを終了します")
+    }
+}
+Task.detached {
+    do {
+        for try await element in asyncStream {
+            print("3つ目 \(element)")
+        }
+    } catch {
+        print("3つ目に\(error)が発生したのでループを終了します")
+    }
+}
 
 
 Task.detached {
     await asyncStream.send(Int.random(in: 0...99))
     await asyncStream.send(Int.random(in: 0...99))
-    await asyncStream.send(Int.random(in: 0...99))
 
     try await Task.sleep(nanoseconds: 1_000_000_000)
     await asyncStream.send(Int.random(in: 0...99))
+}
+Task.detached {
+    await asyncStream.send(Int.random(in: 0...99))
+
+    try await Task.sleep(nanoseconds: 1_000_000_000)
     await asyncStream.send(Int.random(in: 0...99))
 }
 
